d9c14496e2d6749ba2f019ce5b0be50bc83ca5a4
ZahinSam1 <toky.jahin@gmail.com>
2023-01-04 21:37:22 +0600

    editable form added

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ var loadFile = function (event) {
   var image = document.getElementById("output");
   image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+// for edit part
+var editForm = document.getElementByclass("editProfileMenu");
+var updateButton = document.getElementById("btn-submit");
+var isEditable = false;
+
+function Editable() {
+  if (isEditable === true) {
+    editForm.style.display = "block";
+    isEditable = false;
+  } else {
+    editForm.style.display = "none";
+    isEditable = true;
+  }
+}
